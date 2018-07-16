1b896885c10fa587645e2eae3f22c1ef29d95d1b
Mark Rabins <markrabins@gmail.com>
2018-07-16 22:38:37 +0100

    Updated list formatting

--- domains.py
+++ domains.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import read
+import re
 
 def read_domains(file):
     df = file
     domains_df = df["url"]
-    domains = domains_df.value_counts()
+    domains = domains_df.value_counts().nlargest(100)
+    no_sub_list = []
     for name, row in domains.items():
-        print("{0}: {1}".format(name, row))
+        no_sub_domain = re.match(r'(?:\w*://)?(?:.*\.)?([a-zA-Z-1-9]*\.[a-zA-Z]{1,}).*', name)
+        no_sub_list.append(no_sub_domain)
+        return no_sub_list
 
 read_domains(read.load_data())
